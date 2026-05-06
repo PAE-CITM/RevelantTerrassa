ffa833e778811aa43ff73d7ba20e6baf47c60f67
edgarmesad <118179227+edgarmd1@users.noreply.github.com>
2026-05-06 11:37:48 +0200

    OnBoarding Phase 2 - Camera roll fits in camera and invoke event

---
+++ Assets/Scripts/Onboarding/rolltest.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace OnBoarding
+{
+    public class rolltest : MonoBehaviour
+    {
+        public void printTest()
+        {
+            Debug.Log("Roll inserted into the camera");
+        }
+    }
+}
